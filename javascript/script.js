443a366674f0fcfa0b91c65db83fe4526fe176bc
Domi <Dominik.janser@mail.com>
2023-02-10 14:18:55 +0100

    stuff and things

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 //get data from json
 
-const json_string = [{ 
+const json_string = `[{ 
     "taskName": "Restday", 
     "image": "images/restday.jpg", 
     "description": "Regenerate your strength.", 
@@ -27,61 +27,65 @@ const json_string = [{
     "description": "Build more strength.", 
     "importance": 0    
 
-}];
+}]`;
 
 //parse it into object
-let obj_tasks = [{ 
-    taskName: "Restday", 
-    image: "images/restday.jpg", 
-    description: "Regenerate your strength.", 
-    importance: 0    
+let obj_tasks = JSON.parse(json_string);
 
-},
-{ 
-    taskName: "Yoga", 
-    image: "images/yoga.jpg", 
-    description: "Do yoga and stretch, for staying healthier.", 
-    importance: 0    
-
-},
-{ 
-    taskName: "Climbing", 
-    image: "images/climbing.jpg", 
-    description: "Go climbing and train the technique", 
-    importance: 0    
-
-}, 
-{ 
-    taskName: "Weight training", 
-    image: "images/weighttraining.jpg", 
-    description: "Build more strength.", 
-    importance: 0    
-
-}];
-console.log(obj_tasks);
 
 //create html cards
 for(let j = 0; j < obj_tasks.length; j++){
-    document.getElementById("main").innerHTML += `<div class="card" style="width: 18rem;">
+    document.getElementById("main").innerHTML += `
+    <div>
+    <div class="card" style="width: 18rem;">
     <img class="card-img-top" src="${obj_tasks[j].image}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${obj_tasks[j].taskName}</h5>
       <p class="card-text">${obj_tasks[j].description}</p>
-      <p>Priority level: ${obj_tasks[j].importance}</p>
+      <p> Priority level: </p>
+      <div class="priority_container">
+        <p class="priority"> ${obj_tasks[j].importance}</p>
+      </div>
+      <button class="priority_button">+1</button>
+    </div>
     </div>
   </div>`;
+  change_priority_color(j);
 }
-console.log(document.getElementsByClassName("main").innerHTML);
+
+//get a list of buttons
+let list_buttons = document.getElementsByClassName("priority_button");
 
 //add function for button
-function add_priority(){
-
+//function add_priority()
+//sort function
+//function sort_priority(){
+    //obj_tasks
+//}
+//add eventlistener
+//priority color function
+function change_priority_color(i){
+    if(obj_tasks[i].importance < 2){
+        document.getElementsByClassName("priority_container")[i].innerHTML = `<p class="priority bg-success"> ${obj_tasks[i].importance}</p>`;
+    }
+    else if(obj_tasks[i].importance < 4){
+        document.getElementsByClassName("priority_container")[i].innerHTML = `<p class="priority bg-warning"> ${obj_tasks[i].importance}</p>`;
+    }
+    else{
+        document.getElementsByClassName("priority_container")[i].innerHTML = `<p class="priority bg-danger"> ${obj_tasks[i].importance}</p>`;
+    }
 }
 
-//add eventlistener
-/*for(let i = 0; i < obj_tasks.length; i++){
-    obj_tasks[i].addEventListener("click",add_priority());
-}*/
+for(let i = 0; i < list_buttons.length; i++){
+    list_buttons[i].addEventListener("click",function()
+        {
+            obj_tasks[i].importance ++;
+            change_priority_color(i);
 
+            console.log("i pressed the stupid button");
+        });
+    console.log(obj_tasks[i].importance);
+}
 
-
+//sort button
+//sort event listener
